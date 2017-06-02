22b63efd671bafe33c69d93a58145ecd84c42a6b
Teppo Kurki <teppo.kurki@iki.fi>
2017-06-02 22:23:52 +0300

    Replace ADS1115 diff amps with INA219 amps

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var Transform = require('stream').Transform;
 
+var ina219 = require("ina219");
+ina219.init();
+
+const coeff = 0.016361102;
+
+
 function I2cProvider() {
-  setInterval(measure.bind(this, this.push.bind(this)), 5000);
+  const pushIt = this.push.bind(this)
+  ina219.calibrate32V1A(function() {
+    setInterval(measure.bind(this, pushIt), 5000)
+  })
   Transform.call(this, {
     objectMode: true
   });
@@ -13,18 +22,19 @@ var Ads1x15 = require('node-ads1x15');
 var ads1x15 = new Ads1x15(1, 0x48);
 
 function measure(callback) {
-  var ampV = 0;
-  ads1x15.readADCDifferential23(256, 32).then(function(d) {
-    amps = d * 1.894259348;
+  var amps;
+  ina219.getShuntVoltage_raw().then(function(shuntVoltage) {
+    console.log(shuntVoltage)
+    amps = shuntVoltage * coeff;
     return ads1x15.readADCSingleEnded(0, 4096, 250);
-  }).then(function(d) {
+  }).then(function(measuredVolts) {
     callback({
       updates: [{
         "source": {
         },
         values: [{
         path: "electrical.batteries.house.voltage",
-        value: (d / 1000 * 4.11135334)
+        value: (measuredVolts / 1000 * 4.11135334)
       }, {
         path: "electrical.batteries.house.current",
         value: amps
